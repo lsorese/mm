4a3e1bad2fdb1731b99a08cb41fa463a7716969e
Logan Sorese <lsorese@Logans-MacBook-Pro-2.local>
2017-03-14 11:29:18 -0700

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,80 @@
+    jQuery(function() {
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.base__pagination .base__section',
+            paginationClickable: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            keyboardControl: true,
+            mousewheelControl: true,
+            direction: 'vertical',
+            onSlideChangeEnd: function(swiper) {
+                $('.bullet').removeClass('active');
+                $('.bullet[data-index="' + swiper.activeIndex + '"]').addClass('active');
+            },
+            paginationBulletRender: function(swiper, index, className) {
+                return '<div class="bullet" data-name="' + data[index] + '" data-index="' + index + '"><span class="bullet__one"></span><span class="bullet__name"></span><span class="bullet__two" data-name="name"></span></div>';
+            }
+        });
+        $('.bullet[data-index="0"]').addClass("active");
+
+        $('.bullet').each(function(el) {
+            $(this).children('.bullet__name').html($(this).data('name'));
+        });
+
+        $('.bullet').on('click', function() {
+            swiper.slideTo($(this).attr('data-index'), 300);
+        });
+        $('.base__scroll').on('click', function() {
+            swiper.slideNext(300);
+        });
+        $('.scrollArrow').on('click', function() {
+            swiper.slideNext(300);
+        });
+
+        $(".animsition").animsition({
+            inClass: 'fade-in-left',
+            outClass: 'fade-out-right',
+            inDuration: 800,
+            outDuration: 800,
+            linkElement: '.animsition-link',
+            // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+            loading: true,
+            loadingParentElement: 'body', //animsition wrapper element
+            loadingClass: 'animsition-loading',
+            loadingInner: '', // e.g '<img src="loading.svg" />'
+            timeout: false,
+            timeoutCountdown: 5000,
+            onLoadEvent: true,
+            browser: ['animation-duration', '-webkit-animation-duration'],
+            // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+            // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+            overlay: false,
+            overlayClass: 'animsition-overlay-slide',
+            overlayParentElement: 'body',
+            transition: function(url) { window.location.href = url; }
+        });
+
+        jQuery('.hamburger').on('click', function(e) {
+            e.preventDefault;
+            $('.hamburger').addClass('hamburger--open');
+        })
+
+        $('.drawer').drawer();
+        $('.drawer').on('drawer.opened', function() {
+            $('.hamburger').addClass('hamburger--open');
+        });
+        $('.drawer').on('drawer.closed', function() {
+            $('.hamburger').removeClass('hamburger--open');
+        });
+        $('.masthead').height($('.base__inside').height() / 2);
+        $('.grid__wrap').height($('.base__inside').height());
+
+        $('.grid-item').height($('.base__inside').height() / 2);
+        $('.grid-item--tall').height($('.base__inside').height());
+
+        $('.grid').isotope({
+            // options
+            itemSelector: '.grid-item',
+            layoutMode: 'masonry'
+        });
+    });
